5b9e5ad20d805627a1443f29d94d043858a91869
ajeetkumarkb02@gmail.com <ajeetkumarkb02@gmail.com>
2025-10-20 21:17:05 +0530

    add splash effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
-
+'use client';
+import SplashCursor from "./SplashCursor";
 
 function App() {
   return (
       <div className="min-h-screen flex flex-col">
+         <SplashCursor />
         <NavBar />
         <Outlet/>
         <Footer/>
